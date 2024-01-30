830668db59d13c0186e9a4f942db87dca6a1813f
David Shaffer <dave.shaffer@gmail.com>
2024-01-30 17:54:07 -0500

    Fix function template path again

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_cloudfront_origin_access_control" "access_control" {
 
 resource "aws_cloudfront_function" "function" {
   code = templatefile(
-    "./function.tftpl",
+    "${path.module}/function.tftpl",
     {
       redirectable_domains = var.redirectable_domains
       domain_name          = var.canonical_domain_name
